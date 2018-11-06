ab1cb67eb5a6915fa2e11a26547983a2f4976e51
Marcelo Galdino <marcelinhomgo@gmail.com>
2018-11-06 12:16:24 -0200

    retorna usuarios por lista, busca, e autentica user e senha

--- index.php
+++ index.php
@@ -3,15 +3,22 @@
 require_once("config.php");
 
 //$sql = new Sql();
-
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
 
-$marcelo = new Usuario();
+//$marcelo = new Usuario();
+//$marcelo->loadByid(4);
+//echo ($marcelo);
 
-$marcelo->loadByid(4);
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo ($marcelo);
+//$busca = Usuario::search("Ma");
+//echo json_encode($busca);
+
+$usuario = new Usuario();
+$usuario->login("user","12345");
+
+echo $usuario;
 
 ?>
